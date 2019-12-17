1056277f9b59d7b7b373b118e2a4d1ffcc2f01ea
sammyebinne <sammyebinne@Samarus-MacBook-Pro.local>
2019-12-17 22:28:19 +0100

    Previous display

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -14,24 +14,34 @@ class App extends Component {
 
   reset = () => {
     this.setState(
-      { result: '0' }
+      { current: '0' }
     );
   }
 
   addToCurrent = (symbol) => {
-    this.setState(
-      { current: this.state.current + symbol }
-    )
+    console.log(symbol);
+    if (['/', '*', '-', '+'].indexOf(symbol) > -1) {
+      let { previous } = this.state;
+      previous.push(this.state.current + ' ' + symbol);
+      this.setState({ previous })
+    } else {
+      if (this.state.current === "0" && symbol !== ".") {
+        this.setState({ current: symbol })
+      } else {
+        this.setState({ current: this.state.current + symbol })
+      }
+    }
   }
 
   render() {
     const buttons = [
-      { symbol: 'C', cols: 3, action: this.reset },
+      { symbol: 'C', cols: 2, action: this.reset },
+      { symbol: '%', cols: 1, action: this.addToCurrent },
       { symbol: '/', cols: 1, action: this.addToCurrent },
       { symbol: '7', cols: 1, action: this.addToCurrent },
       { symbol: '8', cols: 1, action: this.addToCurrent },
       { symbol: '9', cols: 1, action: this.addToCurrent },
-      { symbol: 'X', cols: 1, action: this.addToCurrent },
+      { symbol: '*', cols: 1, action: this.addToCurrent },
       { symbol: '4', cols: 1, action: this.addToCurrent },
       { symbol: '5', cols: 1, action: this.addToCurrent },
       { symbol: '6', cols: 1, action: this.addToCurrent },
@@ -48,10 +58,13 @@ class App extends Component {
     return (
 
       <div className="App">
+        {this.state.previous.length > 0 ?
+          <div className="prev-display">{this.state.previous[this.state.previous.length - 1]}</div>
+          : null}
         <input className="result" type="text" value={this.state.current}></input>
-        <br/>
+        <br />
         {buttons.map((btn, i) => {
-          return <Button key={i} symbol={btn.symbol} cols={btn.cols} action={(symbol)=> btn.action(symbol)}></Button>
+          return <Button key={i} symbol={btn.symbol} cols={btn.cols} action={(symbol) => btn.action(symbol)}></Button>
         })}
       </div>
     )
